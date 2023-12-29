e5993272ef5d28380e4422b724ba45f2c7ef7b2f
“Rungtawan <rungtawan.s@kkumail.com>
2023-12-29 20:17:42 +0700

    add pair project : coffee and tea shop online

---
+++ Project/js/lightsilder.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    
+    $("#lightSlider").lightSlider({
+        item: 6,
+        autoWidth: true,
+        slideMove: 1, // slidemove will be 1 if loop is true
+        slideMargin: 45,
+ 
+        addClass: '',
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+        easing: 'linear', //'for jquery animation',////
+ 
+        speed: 400, //ms'
+        auto: true,
+        loop: true,
+        slideEndAnimation: true,
+        pause: 2500,
+ 
+        keyPress: true,
+        controls: true,
+        prevHtml: '',
+        nextHtml: '',
+ 
+        rtl:false,
+        adaptiveHeight:false,
+ 
+        vertical:false,
+        verticalHeight:500,
+        vThumbWidth:100,
+ 
+        thumbItem:10,
+        pager: true,
+        gallery: false,
+        galleryMargin: 5,
+        thumbMargin: 5,
+        currentPagerPosition: 'middle',
+ 
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        swipeThreshold: 40,
+ 
+        responsive : [],
+ 
+        onBeforeStart: function (el) {},
+        onSliderLoad: function (el) {},
+        onBeforeSlide: function (el) {},
+        onAfterSlide: function (el) {},
+        onBeforeNextSlide: function (el) {},
+        onBeforePrevSlide: function (el) {}
+    });
+});
---
+++ Project/js/nav2.js
@@ -0,0 +1,4 @@
+const selectElement = (element) => document.querySelector(element)
+selectElement('.menu-icon').addEventListener('click', () => {
+    selectElement('nav').classList.toggle('active');
+});
